ad03909394105ac293cabb57f0240a4f901115e1
vishal <vishalprajapat240@gmail.com>
2022-01-05 11:50:00 +0530

    Updated WebRTC

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
 export const environment = {
   production: true,
-  url: 'https://onmedtesting.com/api/Securer/',
-  qrcode: 'https://onmedtesting.com/#/covid-result/',
-  resetpassword: 'https://onmedtesting.com/#/reset-password/'
 };
